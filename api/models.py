84ea860b64666745eadb9041f69781874d5f4576
Farhadzafar <farhadzafari10@gmail.com>
2025-08-19 15:56:21 +0430

    🙌 Add Timestamp model with created_at

--- api/models.py
+++ api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Timestamp(models.Model):
+    created_at = models.CharField(max_length=45)
+    updated_at = models.CharField(max_length=45)
