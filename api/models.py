d0f35a57463e44a06644c17f5c2f1aa999744bac
Marwa Sayed <marwa_sayed88@outlook.com>
2025-08-24 06:49:08 -0700

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
-# Create your models here.
+
+class Currency(models.Model):
+    name = models.CharField(max_length=45, unique=True) 
+    symbol = models.CharField(
+        max_length=3,
+        validators=[MinLengthValidator(3), MaxLengthValidator(3)] 
+    )
+    timestamp = models.ForeignKey(
+        Timestamp, on_delete=models.CASCADE, related_name="currencies"
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.symbol})"
