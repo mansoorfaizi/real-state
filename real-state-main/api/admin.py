37a7444e9da20035f3b2e60dc405bb0dbd202aa4
Marwa Sayed <marwa_sayed88@outlook.com>
2025-08-24 22:53:43 -0700

    update the last changes

--- real-state-main/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models.area import Area
-
-@admin.register(Area)
-class AreaAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "district", "timestamp")   
-    search_fields = ("name",)                                
