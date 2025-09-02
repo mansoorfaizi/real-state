559c669ff69a318dc603c8840d9cd535d1122679
Sakina Rasa <sakina.rasa2020@gmail.com>
2025-09-02 16:23:14 +0430

    Add Review model with rating, message, user, and property relations

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models.area import Area
 from api.models.house import House
+from api.models.review import Review
 
 @admin.register(Area)
 class AreaAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "district", "timestamp")   
     search_fields = ("name",)   
-admin.site.register(House)                            
+admin.site.register(House)  
+admin.site.register(Review)                          
--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,2 +1,3 @@
 from .area import Area
 from api.models.house import House
+from api.models.review import Review
