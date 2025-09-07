301d0b36455266c8264a50b6a4fb867b9b4e3462
Sakina Rasa <sakina.rasa2020@gmail.com>
2025-09-07 22:10:59 +0430

    Update model Meta ordering to use created_at
     Update model Meta ordering to use created_at
    
    Update favorite model

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models.area import Area
 from api.models.house import House
+from api.models.review import Review
+from api.models.favorite import Favorite
 
-@admin.register(Area)
-class AreaAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "district", "timestamp")   
-    search_fields = ("name",)   
-admin.site.register(House)                            
+admin.site.register( Area)   
+admin.site.register(House)  
+admin.site.register(Review) 
+admin.site.register(Favorite)                        
--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,2 +1,4 @@
 from .area import Area
 from api.models.house import House
+from api.models.review import Review
+from api.models.favorite import Favorite
