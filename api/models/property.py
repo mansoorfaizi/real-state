e4dfd12b5c807e2e45217d56b1878d34d89e66d8
Mansoor <mansoorfaizi.cs05@gmail.com>
2025-09-13 09:25:48 +0430

    fixed the property media file

--- api/models/property.py
+++ api/models/property.py
@@ -30,3 +30,16 @@ class Property(Timestamp):
 
     def __str__(self):
         return f"{self.title} - {self.get_deal_type_display()} ({self.get_property_type_display()})"
+
+
+
+class PropertyMedia(Timestamp):
+    file = models.CharField(max_length=45)
+    caption = models.CharField(max_length=45)
+
+    property = models.ForeignKey("Property", on_delete=models.CASCADE)
+    property_area = models.ForeignKey("Area", on_delete=models.SET_NULL, null=True, blank=True)
+
+
+    def __str__(self):
+        return f"{self.caption} ({self.file})"
--- api/models/province.py
+++ api/models/province.py
@@ -1,22 +1,5 @@
-from django.db import migrations, models
+from .timestamp import Timestamp, models
 
-class Migration(migrations.Migration):
 
-    initial = True
-
-    dependencies = [
-    ]
-
-    operations = [
-        migrations.CreateModel(
-            name='Province',
-            fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
-                ('name', models.CharField(max_length=45)),
-                ('timestamp_id', models.IntegerField()),
-            ],
-            options={
-                'db_table': 'province',
-            },
-        ),
-    ]
+class Province(Timestamp):
+    name = models.CharField(max_length=45)
