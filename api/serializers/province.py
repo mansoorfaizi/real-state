c52063bd3560ea2ddcac574dc1689f3ae04b3d15
Marwa Sayed <marwa_sayed88@outlook.com>
2025-09-23 08:42:02 -0700

    Add ProvinceSerializer for Province model

---
+++ api/serializers/province.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from api.models.province import Province
+
+class ProvinceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Province
+        fields = "__all__"
